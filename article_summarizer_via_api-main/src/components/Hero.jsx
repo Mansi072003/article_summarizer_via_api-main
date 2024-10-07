57b69e5d6317492d503022f0c114f721af05387f
Mansi072003 <96525261+Mansi072003@users.noreply.github.com>
2024-10-08 01:03:03 +0530

    Add files via upload

---
+++ article_summarizer_via_api-main/src/components/Hero.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        {/* <img src="https://toppng.com/uploads/preview/peach-aviation-airline-logo-vector-11573946623b86lfbucej.png" alt='sumz_logo' className='w-28 object-contain' /> */}
+
+        <button
+          type='button'
+          onClick={() =>
+            window.open("https://github.com/v-Star55/article_summarizer_via_api", "_blank")
+          }
+          className='black_btn'
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient '>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
